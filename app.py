a7ef1780795a57b3c9f87a4a09c1c03421ea8a08
lyfer233 <2217636939@qq.com>
2021-08-06 23:41:05 +0800

    First submit~

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from Left_Bar import Ui_MainPage
+from PyQt5.Qt import *
+from layout import Ui_MainPage
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainwindow = QMainWindow()
-    ui = Ui_MainPage()
-    ui.setupUi(mainwindow)
+    Layout = Ui_MainPage()
+    Layout.setupUi(mainwindow)
     mainwindow.show()
     sys.exit(app.exec_())
